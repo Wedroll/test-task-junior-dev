cca24e0745aba33b6fe7367a7f9e3d9b40154236
lvfe frh <ilya.gruzinskiy@bk.ru>
2025-06-15 00:24:40 +0300

    Добавлены методы isSorted, canPour и pour

--- task1-liquid-sorting/src/index.js
+++ task1-liquid-sorting/src/index.js
@@ -1,17 +1,52 @@
 class LiquidSorter {
     constructor(tubes, capacity) {
-        this.tubes = JSON.parse(JSON.stringify(tubes));
+        this.tubes = JSON.parse(JSON.stringify(tubes)); 
         this.capacity = capacity;
         this.numTubes = tubes.length;
         this.moves = [];
     }
 
-  
     isSorted() {
-        return false; 
+        for (let i = 0; i < this.numTubes; i++) {
+            const tube = this.tubes[i];
+            if (tube.length > 0) {
+                const color = tube[0];
+                for (let j = 1; j < tube.length; j++) {
+                    if (tube[j] !== color) return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    getEmptySpace(tube) {
+        return this.capacity - tube.length;
+    }
+
+    canPour(fromTube, toTube) {
+        if (fromTube.length === 0 || this.getEmptySpace(toTube) === 0) return false;
+        const fromColor = fromTube[fromTube.length - 1];
+        const toColor = toTube.length > 0 ? toTube[toTube.length - 1] : null;
+        return toColor === null || toColor === fromColor;
+    }
+
+    pour(fromIdx, toIdx) {
+        const fromTube = this.tubes[fromIdx];
+        const toTube = this.tubes[toIdx];
+        const space = this.getEmptySpace(toTube);
+        const pourAmount = Math.min(space, fromTube.length);
+
+        const color = fromTube[fromTube.length - 1];
+        let count = 0;
+        for (let i = fromTube.length - 1; i >= 0 && fromTube[i] === color && count < pourAmount; i--) {
+            count++;
+        }
+
+        const moved = fromTube.splice(fromTube.length - count, count);
+        toTube.push(...moved);
+        this.moves.push(`(${fromIdx}, ${toIdx})`);
     }
 }
-
 
 const tubes = [
     [2, 1, 10, 5, 6, 12], [4, 8, 2, 3, 7, 12],
@@ -22,3 +57,4 @@ const capacity = 6;
 
 const sorter = new LiquidSorter(tubes, capacity);
 console.log("Sorter initialized with", sorter.numTubes, "tubes.");
+console.log("Is sorted:", sorter.isSorted());
